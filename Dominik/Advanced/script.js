022d9e5198a4eef06894f0765eb30a02fc7bed8f
Dominik Burycz <dominik.burycz@gmail.com>
2024-01-31 15:04:19 +0100

    added space

--- Dominik/Advanced/script.js
+++ Dominik/Advanced/script.js
@@ -1,7 +1,7 @@
 function timeConvert (minutesInH) {
   let hours = Math.floor(minutesInH / 60)
   let minutes = minutesInH % 60
-  return `${minutesInH} minutes =${hours} hour(s) and ${minutes} minute(s)`
+  return `${minutesInH} minutes = ${hours} hour(s) and ${minutes} minute(s)`
 }
 
 document.write(timeConvert(200));
