2d553636084b504ec58a1a1c22529f8772aa3f25
Dominik Burycz <dominik.burycz@gmail.com>
2024-01-31 14:50:38 +0100

    completed all intermediate exercises

--- Dominik/Intermediate/script.js
+++ Dominik/Intermediate/script.js
@@ -1,4 +1,5 @@
 //Capitalize ---------------------------------------------------------
+document.write("Exercise 1 <hr><br>")
 
 let capitalize = function(text) {
   return text.charAt(0).toUpperCase() + text.slice(1);
@@ -7,3 +8,15 @@ let capitalize = function(text) {
 let inputString = "i am a web dev";
 let capitalizedString = capitalize(inputString);
 document.write(capitalizedString);
+
+document.write("<br><br><br>")
+//Average Grade ------------------------------------------------------
+document.write("Exercise 2 <hr><br>")
+
+function averageGrade (math, physics, english) {
+  let sum = (math + physics + english);
+  let average = (sum / averageGrade.length);
+  document.write(`Sum: ${sum}<br>Average: ${average}`)
+}
+
+averageGrade(5, 3, 1);
